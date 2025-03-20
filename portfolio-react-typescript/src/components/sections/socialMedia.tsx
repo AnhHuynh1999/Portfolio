32f8b543b9e8075927274ade1127b1b91ca4caa0
Anh Huỳnh <huynhbaoanh1011@gmail.com>
2025-03-20 23:10:23 +0700

    update social

--- portfolio-react-typescript/src/components/sections/socialMedia.tsx
+++ portfolio-react-typescript/src/components/sections/socialMedia.tsx
@@ -5,19 +5,19 @@ import { Link } from 'react-router-dom'
 const Socialmedia = () => {
   return (
     <div className='my-4 d-flex items-center gap-3'>
-      <Link className='highlight' to={APP_DATA.YOUTUBE_URL}>
+      <Link className='highlight' target='_blank' to={APP_DATA.YOUTUBE_URL}>
         <FaYoutube size={30} />
       </Link>
-      <Link className='highlight' to={APP_DATA.GITHUB_URL}>
+      <Link className='highlight' target='_blank' to={APP_DATA.GITHUB_URL}>
         <FaGithub size={30} />
       </Link>
-      <Link className='highlight' to={APP_DATA.FACEBOOK_URL}>
+      <Link className='highlight' target='_blank' to={APP_DATA.FACEBOOK_URL}>
         <FaFacebook size={30} />
       </Link>
-      <Link className='highlight' to={APP_DATA.INSTAGRAM_URL}>
+      <Link className='highlight' target='_blank' to={APP_DATA.INSTAGRAM_URL}>
         <FaInstagram size={30} />
       </Link>
-      <Link className='highlight' to={APP_DATA.TIKTOK_URL}>
+      <Link className='highlight' target='_blank' to={APP_DATA.TIKTOK_URL}>
         <FaTiktok size={30} />
       </Link>
     </div>
